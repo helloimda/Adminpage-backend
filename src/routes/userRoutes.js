540ba6ecef6bbca68131e51a96778cdc782ed1ef
minseok <149202694+helloimda@users.noreply.github.com>
2024-08-20 21:47:07 +0900

    Fix: 유저 검색기능 규격 수정

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/members/:page', userController.getMembers);
-router.get('/members/search/id/:name', userController.searchMembersById);
-router.get('/members/search/nick/:name', userController.searchMembersByNick);
+router.get('/members/search/id/:name/:page', userController.searchMembersById);
+router.get('/members/search/nick/:name/:page', userController.searchMembersByNick);
 router.post('/members/ban/:mem_idx', userController.banUser);
 router.post('/members/delete/:mem_idx', userController.deleteUser);
 router.get('/members/userdetail/:mem_idx', userController.getUserDetail);
